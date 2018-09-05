9cbc9611334a50fee210a28ffac8af3c623f6afc
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-09-05 18:34:33 +0200

    tweets endpoint is now working

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const consumerApiSecretKey = 'vemUutwZ6BM5XjRLjpwlfBZ0nxoLd4J9KOYQh6S0npnRrRFU20
 const consumerAccessToken = '539951298-OSsgK7Jtt96T55FDTLR7nXzXYXdRs2eLmeRjaPT6';
 const consumerAccessTokenSecret = 'MxgtPDmuRPQ48ZdTgMIsp4G9UEDMLDXCb65FnkkqZAysl';
 
-export {consumerApiKey, consumerApiSecretKey, consumerAccessToken, consumerAccessTokenSecret};
+module.exports = { consumerApiKey, consumerApiSecretKey, consumerAccessToken, consumerAccessTokenSecret };
--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-console.log('hello');
+const getTweets = require('./twitter');
+const express = require('express');
+const app = express();
 
-require('http')
-  .Server((_,r)=>
-r.end('We are running in separate file now')).listen();
+
+var message = "";
+
+promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+  }, 2000)
+});
+
+promise1.then(msg => console.log(msg));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/query/:search', (req, res) => {
+ if(req.params && req.params.search){
+   /*return new Promise(resolve => {
+     resolve(getTweets(req.params.search).then(data => res.send(data)));
+   });*/
+   getTweets(req.params.search).then(data => res.send(data.map(data => data.text)));
+ }
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
