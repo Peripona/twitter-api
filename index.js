827a46740c65add19519f763ae86607008a49df5
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-09-06 12:17:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const getTweets = require('./twitter');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World! Auto deployed with Now now.'));
+app.get('/', (req, res) => res.send('Hello World! Auto deployed with Now now. Only working with PR though'));
 
 app.get('/query/:search', (req, res) => {
  if(req.params && req.params.search){
